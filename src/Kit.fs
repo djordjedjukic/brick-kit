ecb6ea354ee25153455d8ae8e226935390d82489
Djordje Djukic <djukicdjole@gmail.com>
2024-01-27 21:16:30 +0100

    add type providers for all csv files

--- src/Kit.fs
+++ src/Kit.fs
@@ -2,7 +2,18 @@ module Kit
 
 open FSharp.Data
 
+type ThemesTypeProvider = CsvProvider<Sample="../data/themes.csv">
+type SetsTypeProvider = CsvProvider<Sample="../data/sets.csv">
+type InventorySetsTypeProvider = CsvProvider<Sample="../data/inventory_sets.csv">
+type InventoriesTypeProvider = CsvProvider<Sample="../data/inventories.csv">
+type CategoriesTypeProvider = CsvProvider<Sample="../data/part_categories.csv">
+type InventoryPartsTypeProvider = CsvProvider<Sample="../data/inventory_parts.csv">
 type PartsTypeProvider = CsvProvider<Sample="../data/parts.csv">
+type PartRelationshipsTypeProvider = CsvProvider<Sample="../data/part_relationships.csv">
+type ElementsTypeProvider = CsvProvider<Sample="../data/elements.csv">
+type MiniFigsTypeProvider = CsvProvider<Sample="../data/minifigs.csv">
+type InventoryMiniFigsTypeProvider = CsvProvider<Sample="../data/inventory_minifigs.csv">
+type ColorsTypeProvider = CsvProvider<Sample="../data/colors.csv">
 let parts = PartsTypeProvider.GetSample().Rows
 
 let printFirst10Parts () =
