4fa8b8bd1f6b0ababd13579c5ff3e4242a7a6757
Djordje Djukic <djukicdjole@gmail.com>
2024-01-28 20:44:28 +0100

    wip

--- src/Data.fs
+++ src/Data.fs
@@ -2,17 +2,45 @@ module Data
 
 open FSharp.Data
 
-type Theme = {
+type Color = { Name: string; RGB: string }
+
+type Category = {
     Id: int
+    Name: string
+    Parts: Part list
+}
+
+and Part = {
+    Category: Category
+    Number: string
     Name: string
 }
 
-type Set = {
+type InventoryPart = {
+    Number: string
+    Color: Color
+    IsSpare: bool
+}
+
+type Inventory = {
+    Id: int
+    Version: int
+    Parts: InventoryPart list
+}
+
+type Theme = {
+    Id: int
+    Name: string
+    Sets: Set list
+}
+
+and Set = {
     Number: string
     Name: string
     Year: int
-    ThemeId: int
+    Theme: Theme
     NumberOfParts: int
+    Inventories: Inventory list
 }
 
 type MiniFig = {
@@ -21,26 +49,10 @@ type MiniFig = {
     NumberOfParts: int
 }
 
-type Color = {
-    Name: string
-    RGB: string
-}
-
-type Part = {
-    Category: string
-    Number: string
-    Name: string
-    Color: Color
-}
-
-type Inventory = {    
-    Parts: Part list
-    Quantity: int
-    IsSpare: bool   
-}
-
 type ThemesTypeProvider = CsvProvider<"../data/themes.csv">
+let themesData = ThemesTypeProvider.Load("../data/themes.csv")
 type SetsTypeProvider = CsvProvider<"../data/sets.csv">
+let setsData = SetsTypeProvider.Load("../data/sets.csv")
 type InventorySetsTypeProvider = CsvProvider<"../data/inventory_sets.csv">
 type InventoriesTypeProvider = CsvProvider<"../data/inventories.csv">
 type CategoriesTypeProvider = CsvProvider<"../data/part_categories.csv">
@@ -52,14 +64,34 @@ type MiniFigsTypeProvider = CsvProvider<"../data/minifigs.csv">
 type InventoryMiniFigsTypeProvider = CsvProvider<"../data/inventory_minifigs.csv">
 type ColorsTypeProvider = CsvProvider<"../data/colors.csv">
 
-let themes =
-    ThemesTypeProvider.GetSample().Rows
-    |> Seq.map (fun t -> { Id = t.Id; Name = t.Name })
 
+let themes =
+    themesData.Rows
+    |> Seq.map (fun t -> {
+        Id = t.Id
+        Name = t.Name
+        Sets =
+            setsData.Rows
+             |> Seq.filter (fun s -> s.Theme_id = t.Id)
+             |> Seq.map (fun s -> {
+                 Number = s.Set_num
+                 Name = s.Name
+                 Year = s.Year
+                 Theme = { Id = t.Id; Name = t.Name; Sets = [] }
+                 NumberOfParts = s.Num_parts
+                 Inventories = []
+             })
+             |> Seq.toList
+             
+    })
+    
 let sets =
     SetsTypeProvider.GetSample().Rows
-    |> Seq.map (fun s -> { Number = s.Set_num
-                           Name = s.Name
-                           Year = s.Year
-                           ThemeId = s.Theme_id
-                           NumberOfParts = s.Num_parts })
+    |> Seq.map (fun s -> {
+        Number = s.Set_num
+        Name = s.Name
+        Year = s.Year
+        Theme = themes |> Seq.find (fun t -> t.Id = s.Theme_id)
+        NumberOfParts = s.Num_parts
+        Inventories = []
+    })
--- src/Kit.fs
+++ src/Kit.fs
@@ -4,12 +4,24 @@ open Data
 
     [<RequireQualifiedAccess>]    
     module Themes =
-        let byName (name: string) =
+        let   (name: string) =
             themes
-            |> Seq.filter (fun part -> part.Name = name)
-            |> Seq.toList
-
-
-
-    
-       
+            |> Seq.filter (fun part -> part.Name.Contains name)
+            
+        let sets = 
+            sets
+            |> Seq.map (fun part -> part.Theme)
+            
+    [<RequireQualifiedAccess>]    
+    module Sets =
+        let byName (name: string) =
+            sets
+            |> Seq.filter (fun part -> part.Name.Contains name)
+            
+        let byTheme (themeName: string) =
+            sets
+            |> Seq.filter (fun part -> part.Theme.Name = Themes.byName themeName |> Seq.head.Id)
+            
+            
+            
+            
